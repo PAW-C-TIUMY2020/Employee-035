4651bd97836225912685e2c0e9a2709607722ce5
poetra-As <62783856+poetra-As@users.noreply.github.com>
2020-10-19 18:16:11 +0700

    Add project files.

---
+++ Employee-035/Models/EmployeeContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Employee-035.Models
+{
+    public class EmployeeContext : DbContext
+{
+    public EmployeeContext(DbContextOptions<EmployeeContext> options) :
+   base(options)
+    { }
+    public DbSet<Employees> Employees { get; set; }
+}
+{
+}
+}
---
+++ Employee-035/Models/Employees.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Employee-035.Models
+{
+    public class Employees
+    public int EmployeeId { get; set; }
+[Column(TypeName = "nvarchar(250)")]
+[Required(ErrorMessage = "This field is required.")]
+[DisplayName("Full Name")]
+public string FullName { get; set; }
+[Column(TypeName = "varchar(10)")]
+[DisplayName("Emp. Code")]
+public string EmpCode { get; set; }
+[Column(TypeName = "varchar(100)")]
+public string Position { get; set; }
+[Column(TypeName = "varchar(100)")]
+[DisplayName("Office Location")]
+public string OfficeLocation { get; set; }
+{
+}
+}
